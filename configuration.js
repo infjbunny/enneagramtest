42c1b3f61950629032c1a031ad0511d0c0d13c18
infjbunny <74402709+infjbunny@users.noreply.github.com>
2020-11-17 05:07:50 -0600

    Update configuration.js
    Added in additional results text

--- configuration.js
+++ configuration.js
@@ -9,530 +9,530 @@ questions = [
     {
         "description": "Has a perfectly good idea, but doesn't share it in case it could be seen as rude.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
         "description": "Is very specific when describing something.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
         "description": "Says, \"I don't believe that\".",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
         "description": "Believes they could be friends with anybody.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
         "description": "Thinks it is bad/unhealthy to hate people.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
         "description": "Doesn't care enough to hate anyone.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
 
         }
     },
     {
         "description": "Has done something morally bad in the past and is not ashamed of it.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
         "description": "Rarely inwardly feels the same as projected outwardly.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
        "description": "Has been labeled as \"offensive\".",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
        "description": "Little inner sense of identity.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
        "description": "Rejection of morality and prejudices when reasoning.",
         "weights": {
-            "Fi": 4
+            "Fi (ExTP)": 4
         }
     },
     {
        "description": "Says, \"I said that the wrong way\".",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Has many different ideas but lacks a logical structure in them.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Will accidentally say something that contradicts the previous idea while trying to point out the big picture.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "When someone opposes them, they will frequently say, \"you're right, but I'm right too\".",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Thinks others focus on unimportant details often.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Quickly loses interest.",
         "weights": {
-            "Ti": 4
+            "Ti(ExFP)": 4
         }
     },
     {
       "description": "Would rather work quickly at the sake of accuracy.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Regrets most choices.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Wouldn't enjoy discussing a theory if it is not connected to reality as they know it.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Suspicious of the validity of overly complex theories.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Uncomfortable sharing sources of their knowledge with others.",
         "weights": {
-            "Ti": 4
+            "Ti (ExFP)": 4
         }
     },
     {
       "description": "Finds others to be lazy.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Insecure about physical appearance",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
       "description": "Frequently forgets important details but will remember small ones.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
      "description": "Could be described as a \"workaholic\".",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
       "description": "Thinks others are too concerned with being comfortable.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Often unaware of their physical health.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Thinks others live in the past too much.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Suspicious of the validity of traditions.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Has heard they dress in inappropriate weather attire. (ex: A t-shirt when it is snowing.)",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Often forgets to eat.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Feels physically uncomfortable but does not prioritize remedying this.",
         "weights": {
-            "Si": 4
+            "Si (ENxJ)": 4
         }
     },
     {
        "description": "Has difficulty understanding metaphors.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Struggles understanding sarcasm.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Says, \"I didn't know that. I wouldn't have done that if they told me so\".",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Pretends to know what someone is talking about.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Struggles to achieve desired results without instructions.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Has been described as \"ditzy\".",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Has difficulty imagining possible consequences.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Could be described as traditional.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Becomes stressed when under time restraints.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Dislikes hypothetical future-based ideas.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Frequently too early or late to appointments.",
         "weights": {
-            "Ni": 4
+            "Ni (ESxJ)": 4
         }
     },
     {
        "description": "Says, \"sorry, I wasn't paying attention\".",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Has difficulty making decisions quickly.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Has issues with motivation.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Believes a lot of things are out of their control.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Frequent bouts of apathy.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Suspicious when taking things at face value. Frequently says, \"nothing is this simple/easy\".",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Finds others to be too concerned with material items.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Could be described as cautious.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Finds others to be too demanding.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Tends to zone out.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Feels incompetent/uncomfortable as an authority figure.",
         "weights": {
-            "Se": 4
+            "Se (INxP)": 4
         }
     },
     {
        "description": "Dislikes brainstorming over various ideas.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Believes others are too concerned with various possibilities and should instead focus on what is happening currently.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Says, \"what should I do now?\".",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Prefers to keep ideas to themselves.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Could be called conventional.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Needs to see facts and proof first, before hearing a theory out.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Could be described as \"set in their ways\".",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Difficulty adapting to changes in plans.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Could be described as strict.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "For better or worse - commits to their decisions completely.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Dislikes open-ended questions.",
         "weights": {
-            "Ne": 4
+            "Ne (ISxP)": 4
         }
     },
     {
        "description": "Often cancels plans due to poor scheduling.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Could be described as a perfectionist.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Would say they are organized, though others would argue the system is inefficient.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Difficulty taking in data until they organize it.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Issue just \"dealing with the facts\" if they are unable to relate it to a system.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Says, \"objectively I should do ____, but...\".",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Unable to not take criticism personally.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Procrastinates doing work until they have a detailed plan and system for it.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Scared of others knowing they are wrong about anything.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Distrusts reality.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Elaborates too much on things others might deem unimportant. Feels that all facts are relevant.",
         "weights": {
-            "Te": 4
+            "Te (IxFJ)": 4
         }
     },
     {
        "description": "Issues not treating others' problems practically in the same way they would their own.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Sporadic lash outs/bouts of emotion.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Could be described as insensitive.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Difficulty reading emotional atmosphere.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Has been called \"self-centered\".",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Distrusts social niceties.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Thinks people are too concerned with emotions of others.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Could be described as \"impolite\".",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Issues understanding how their actions/words could affect others.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Prefers serious conversations most of the time.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
     {
        "description": "Difficulties joking with others.",
         "weights": {
-            "Fe": 4
+            "Fe (IxTJ)": 4
         }
     },
 ]
